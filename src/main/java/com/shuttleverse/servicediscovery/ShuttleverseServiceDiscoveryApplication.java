d26ee9b4407861628bac4ebb44d7d28594390ffb
QW66666 <wusteven0829@gmail.com>
2025-04-12 13:15:04 -0400

    initial setup

---
+++ src/main/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.shuttleverse.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttleverseServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttleverseServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuttleverse.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttleverseServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
