c2df700508ae213b3ff2451ccf76840fd0ac3c8c
QW66666 <wusteven0829@gmail.com>
2025-04-12 13:16:00 -0400

    initial configuration

--- src/main/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplication.java
+++ src/main/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.shuttleverse.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ShuttleverseServiceDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShuttleverseServiceDiscoveryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ShuttleverseServiceDiscoveryApplication.class, args);
+  }
 
 }
--- src/test/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplicationTests.java
+++ src/test/java/com/shuttleverse/servicediscovery/ShuttleverseServiceDiscoveryApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShuttleverseServiceDiscoveryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
